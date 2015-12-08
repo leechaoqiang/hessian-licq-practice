a116ade9fd28d1470b0ae497c047441f23a75476
leechaoqiang <Aristotlelee@126.com>
2015-12-08 18:11:15 +0800

    update

--- hessian-server/src/main/java/com/licq/hessian/service/impl/WeatherServiceImpl.java
+++ hessian-server/src/main/java/com/licq/hessian/service/impl/WeatherServiceImpl.java
@@ -1,13 +1,15 @@
 package com.licq.hessian.service.impl;
 
+import java.util.Random;
+
 import com.licq.hessian.service.WeatherService;
 
 public class WeatherServiceImpl implements WeatherService{
 	
 	static String[] weatherStats = new String[]{"晴","小雨","中雨","大雨","雷阵雨","阴转多云","小雪","中雪","大雪","台风"}; 
 	public String queryWeatherInfo(String cityName) {
-		
-		return null;
+		int  i = new Random().nextInt(10);
+		return cityName+",天气"+weatherStats[i];
 	}
 
 }
